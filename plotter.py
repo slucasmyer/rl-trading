9bb65adc036f4c47852471db70bc89ade9b7b305
Alexander Licato <aalicato@gmail.com>
2024-02-20 11:54:04 -0800

    Added plot functions

--- plotter.py
+++ plotter.py
@@ -1,30 +1,46 @@
-# an example of how to plot in real time.
-# might be useful for exploring training sessions since they take forever to run.
+import time
+import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-import numpy as np
-import time
 
-plt.ion()# Turn on interactive mode
+def multi_objective_scatter(profit_data: list, drawdown_data: list, show: bool = True):
+    """
+    Expects lists of lists of profit and drawdown data for variable generations of individuals.
+    """
+    plt.xlabel("Profit")
+    plt.ylabel("Drawdown")
+    plt.title("Multi-objective Scatter")
+    for i in range(len(profit_data)):
+        plt.scatter(profit_data[i], drawdown_data[i], label="Gen %s" % i)
+    # Add timestamp and gen numbers?
+    plt.savefig("Figures/multi_obj_scatter.png")
+    if show:
+        plt.legend()
+        plt.show()
 
-fig, ax = plt.subplots() # Create a figure and axis
+def univariate_time_series(step_data: list, variable: str, show: bool = True):
+    """
+    Expects list of daily measurements for identified variable. 
+    """
+    days = range(1, len(step_data) + 1)
+    x = days
+    y = step_data
+    plt.plot(x, y)
+    plt.xlabel("Day")
+    plt.ylabel(variable)
+    plt.xticks(days)
+    plt.savefig("Figures/" + variable + "_time_series.png")
+    if show:
+        plt.show()
 
-x, y = [], []
-line, = ax.plot(x, y, 'r-')
 
-for i in range(100):
-    x.append(i)
-    y.append(np.random.rand())
-    
-    line.set_xdata(x)
-    line.set_ydata(y)
-    
-    ax.set_xlim(0, 100)
-    ax.set_ylim(0, 1)
-    
-    fig.canvas.draw()
-    fig.canvas.flush_events()
-    time.sleep(0.1)
-
-plt.ioff() # Turn off interactive mode
+if __name__ == '__main__':
+    profit1_data = [[12000, 15000, 2000, 30, 10000]]
+    drawdown1_data = [[58000, 20000, 32000, 2000, 1000]]
+    multi_objective_scatter(profit1_data, drawdown1_data)
+    profit2_data = [[12000, 15000, 2000, 30, 10000],
+                   [19000, 18000, 12000, 3000, 8000]]
+    drawdown2_data = [[58000, 20000, 32000, 2000, 1000],
+                     [20000, 98000, 1000, 5000, 52000]]
+    multi_objective_scatter(profit2_data, drawdown2_data)
+    univariate_time_series(profit1_data[0], "Profit")
