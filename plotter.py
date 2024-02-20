e292b8bbdba82fc5b2c78d2cc5dc66f293bdcfb9
Alexander Licato <aalicato@gmail.com>
2024-02-20 12:42:58 -0800

    Improved naming for saved plots

--- plotter.py
+++ plotter.py
@@ -3,24 +3,31 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 
-def multi_objective_scatter(profit_data: list, drawdown_data: list, show: bool = True):
+
+def multi_objective_scatter(profit_data: dict, drawdown_data: dict, show: bool = True) -> None:
     """
-    Expects lists of lists of profit and drawdown data for variable generations of individuals.
+    Expects dict of lists of profit and drawdown data indexed by generation. 
     """
+    sorted_gens = sorted(profit_data.keys())
+    gens_covered = str(sorted_gens[0])
     plt.xlabel("Profit")
     plt.ylabel("Drawdown")
-    plt.title("Multi-objective Scatter")
-    for i in range(len(profit_data)):
-        plt.scatter(profit_data[i], drawdown_data[i], label="Gen %s" % i)
-    # Add timestamp and gen numbers?
-    plt.savefig("Figures/multi_obj_scatter.png")
+    plt.title("Profit vs. Drawdown")
+    for gen in sorted_gens:
+        plt.scatter(profit_data[gen], drawdown_data[gen], label="Gen %s" % gen)
+    if len(sorted_gens) > 1:
+        gens_covered += "-%s" % max(sorted_gens)
+    plt.savefig("Figures/%s_multi_obj_scatter_gen_%s.png" %
+                (timestamp(), gens_covered))
     if show:
-        plt.legend()
+        if len(profit_data) > 1:
+            plt.legend()
         plt.show()
 
-def univariate_time_series(step_data: list, variable: str, show: bool = True):
+
+def univariate_time_series(step_data: list, variable: str, gen_pop: tuple, show: bool = True) -> None:
     """
-    Expects list of daily measurements for identified variable. 
+    Expects list of daily measurements for variable associated with solution identified by gen_pop. 
     """
     days = range(1, len(step_data) + 1)
     x = days
@@ -29,18 +36,36 @@ def univariate_time_series(step_data: list, variable: str, show: bool = True):
     plt.xlabel("Day")
     plt.ylabel(variable)
     plt.xticks(days)
-    plt.savefig("Figures/" + variable + "_time_series.png")
+    # Add gen and individual number?
+    plt.savefig("Figures/%s_%s_time_series_gen_%s_pop_%s.png" %
+                (timestamp(), variable, gen_pop[0], gen_pop[1]))
     if show:
         plt.show()
 
 
+def stop_loss_triggered(step_data: list, identifier: list) -> None:
+    """
+    Takes list of stop loss triggered step data for individual. 
+    """
+    pass
+
+
+def timestamp():
+    return str(int(time.time()))
+
+
 if __name__ == '__main__':
-    profit1_data = [[12000, 15000, 2000, 30, 10000]]
-    drawdown1_data = [[58000, 20000, 32000, 2000, 1000]]
+
+    profit1_data = {1: [12000, 15000, 2000, 30, 10000]}
+    drawdown1_data = {1: [58000, 20000, 32000, 2000, 1000]}
+
     multi_objective_scatter(profit1_data, drawdown1_data)
-    profit2_data = [[12000, 15000, 2000, 30, 10000],
-                   [19000, 18000, 12000, 3000, 8000]]
-    drawdown2_data = [[58000, 20000, 32000, 2000, 1000],
-                     [20000, 98000, 1000, 5000, 52000]]
+
+    profit2_data = {1: [12000, 15000, 2000, 30, 10000],
+                    2: [19000, 18000, 12000, 3000, 8000]}
+    drawdown2_data = {1: [58000, 20000, 32000, 2000, 1000],
+                      2: [20000, 98000, 1000, 5000, 52000]}
+
     multi_objective_scatter(profit2_data, drawdown2_data)
-    univariate_time_series(profit1_data[0], "Profit")
+
+    univariate_time_series(profit1_data[1], "profit", (1, 2), False)
