3d357cccabc788cb2d45365c36b539444ae0a6dd
Andrew Perez <perez.andrew028@gmail.com>
2024-02-25 21:34:49 -0800

    Changed "date" column to "timestamp"

--- yahoo_fin_data.py
+++ yahoo_fin_data.py
@@ -5,6 +5,7 @@ def make_csv():
     """Pulls TQQQ data from Yahoo! Finance with the specified start and end date, drops the 'ticker' column and saves
     the data frame as a CSV."""
     raw_df = stock_info.get_data(ticker="TQQQ", start_date="2011-01-01", end_date="2023-12-31", index_as_date=False)
+    raw_df.rename(columns={"date": "timestamp"}, inplace=True)
     raw_df.drop(columns=["ticker"], inplace=True)
     raw_df.to_csv("raw_tqqq_data.csv", index=False)
 
